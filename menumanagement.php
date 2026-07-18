3bcc588d240a66a56184086fa51fa5088ace37d8
Einzenn12 <zenn_ham@dlsu.edu.ph>
2026-07-18 22:43:41 +0800

    Update menumanagement.php

--- menumanagement.php
+++ menumanagement.php
@@ -138,7 +138,7 @@ $totalPages   = 11;
                 <i class="fa-solid fa-gear"></i>
             </button>
 
-            <button class="new-order-btn">
+            <button class="new-order-btn" id="openAddModal">
                 + New Order
             </button>
 
