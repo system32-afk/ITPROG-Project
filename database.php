2a1a68288bdfc4b0bef649abd5f2c67817470755
system32-afk <johngabrielsalamera@gmail.com>
2026-08-12 20:41:35 +0800

    Update database.php

--- database.php
+++ database.php
@@ -1,13 +1,6 @@
 <?php
 mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-//CREDENTIALS
 
-$db_server = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "pabili_db";
-$conn = "";
-$db_port = 3309;
 
 try {
     $conn = new mysqli(
