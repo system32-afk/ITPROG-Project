3a477ebfcb303c7f8609f8520666f5458971ec14
system32-afk <johngabrielsalamera@gmail.com>
2026-08-15 01:04:18 +0800

    ui fix for confirmed order

--- menu.php
+++ menu.php
@@ -509,17 +509,6 @@ $items = $getMenuItems->get_result()->fetch_all(MYSQLI_ASSOC);
 
                     </div>
 
-                    <div class="success-row">
-
-                        <span>
-                            Total paid:
-                        </span>
-
-                        <strong id="successOrderTotal">
-                            ₱0.00
-                        </strong>
-
-                    </div>
 
                 </div>
 
